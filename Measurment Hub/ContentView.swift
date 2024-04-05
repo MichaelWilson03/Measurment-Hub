80d85a53645106da76f361252fdb43386c31eb90
Michae <michaelbwilson2012@gmail.com>
2024-04-05 10:49:06 -0600

    Initial Commit

---
+++ Measurment Hub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Measurment Hub
+//
+//  Created by Michael Wilson on 4/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Measurment Hub/Measurment_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Measurment_HubApp.swift
+//  Measurment Hub
+//
+//  Created by Michael Wilson on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Measurment_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
